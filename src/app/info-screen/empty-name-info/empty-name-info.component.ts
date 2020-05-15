047c596e0bee9a64bdff6d1660780f921709ad8f
Niesobski, Bartosz <bartosz.niesobski@sap.com>
2020-05-15 17:00:29 +0200

    Feature: suggesting name implemented

--- src/app/info-screen/empty-name-info/empty-name-info.component.ts
+++ src/app/info-screen/empty-name-info/empty-name-info.component.ts
@@ -1,21 +1,45 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {HeroService} from "../../shared/hero.service";
 import {Router} from "@angular/router";
+import {NameService} from "../../shared/name.service";
+import {takeWhile} from "rxjs/operators";
 
 @Component({
   templateUrl: './empty-name-info.component.html',
   styleUrls: ['./empty-name-info.component.scss']
 })
-export class EmptyNameInfoComponent {
+export class EmptyNameInfoComponent implements OnInit, OnDestroy {
   @ViewChild("nameForm", {static: false}) nameForm: NgForm;
+  private componentActive: boolean;
+  private askForLoading: boolean;
 
-  constructor(private heroService: HeroService, private router: Router) {
+  constructor(private heroService: HeroService, private nameService: NameService, private router: Router) {
   }
 
+  ngOnInit() {
+    this.askForLoading = false;
+
+    this.nameService.getHeroName().pipe(takeWhile(() => this.componentActive && this.askForLoading))
+      .subscribe((name: string) => {
+          this.nameForm.form.patchValue({'name': name});
+          this.askForLoading = false;
+        }
+      );
+  }
+
+  ngOnDestroy() {
+    this.askForLoading = false;
+  }
 
   onSuggestName() {
-
+    this.askForLoading = !this.askForLoading;
+    this.nameService.getHeroName().pipe(takeWhile(() => this.askForLoading))
+      .subscribe((name: string) => {
+          this.nameForm.form.patchValue({'name': name});
+          this.askForLoading = false;
+        }
+      );
   }
 
   onSubmit() {
