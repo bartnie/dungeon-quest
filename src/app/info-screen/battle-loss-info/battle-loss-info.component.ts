0a95ccf43af7c913efe8f71ec6f5883ad6fe24c7
Niesobski, Bartosz <bartosz.niesobski@sap.com>
2020-05-15 23:59:45 +0200

    Refactor: unnecessary code removed from battle loss info component

--- src/app/info-screen/battle-loss-info/battle-loss-info.component.ts
+++ src/app/info-screen/battle-loss-info/battle-loss-info.component.ts
@@ -1,29 +1,15 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {DungeonService} from "../../shared/dungeon.service";
-import {EnemyModel} from "../../shared/domain/enemy/enemy.model";
 import {Router} from "@angular/router";
 
 @Component({
   templateUrl: './battle-loss-info.component.html',
   styleUrls: ['./battle-loss-info.component.scss']
 })
-export class BattleLossInfoComponent implements OnInit, OnDestroy {
-  private defeatedEnemy: EnemyModel;
-  private componentActive: boolean;
-
+export class BattleLossInfoComponent {
   constructor(private dungeonService: DungeonService, private router: Router) {
   }
 
-  ngOnInit() {
-    this.componentActive = true;
-    this.dungeonService.oldEnemy.subscribe(
-      (enemy: EnemyModel) => this.defeatedEnemy = enemy
-    );
-  }
-
-  ngOnDestroy() {
-    this.componentActive = false;
-  }
 
   onGoToTavern() {
     this.router.navigate(['tavern']);
