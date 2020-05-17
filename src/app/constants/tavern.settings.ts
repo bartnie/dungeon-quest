b7b83c1e1bbfbe60c42df4641a7716cf576b2941
Niesobski, Bartosz <bartosz.niesobski@sap.com>
2020-05-18 00:57:57 +0200

    Adjustment: training and tavern setting changed

--- src/app/constants/tavern.settings.ts
+++ src/app/constants/tavern.settings.ts
@@ -1,6 +1,6 @@
 export class TavernSettings {
   public static STAMINA_PER_WORK = 15;
-  public static GOLD_PER_WORK = 15;
+  public static GOLD_PER_WORK = 20;
 
   public static STAMINA_PER_SLEEP = 25;
   public static HEALTH_PER_SLEEP = 15;
--- src/app/constants/training.settings.ts
+++ src/app/constants/training.settings.ts
@@ -1,5 +1,5 @@
 export class TrainingSettings {
   public static OFFENCE_PER_TRAINING = 1;
   public static DEFENCE_PER_TRAINING = 1;
-  public static STAMINA_PER_TRAINING = 15;
+  public static STAMINA_PER_TRAINING = 20;
 }
